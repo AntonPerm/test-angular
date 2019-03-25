bb5c2eb000fae7310f7fb42817a803a5edd1598a
TepleninAnton <anton.teplenin@gmail.com>
2019-03-25 23:00:46 +0500

    Fix problem with private methods

--- src/app/components/emails-editor/emails-editor.component.ts
+++ src/app/components/emails-editor/emails-editor.component.ts
@@ -4,31 +4,31 @@ import {IEmail} from '../../types/emailsEditor';
 @Component({
 	selector: 'app-emails-editor',
 	template: `
-		<div>
-			<div class="emailsEditor">
-				<div class="header">Share <strong>Board name</strong> with other</div>
-				<div class="emailBlock">
-					<app-email-block
-						*ngFor="let item of emailsArr"
-						[emailObject]="item"
-						(onDeleteClick)="onDeleteClick($event)"
-					></app-email-block>
-					<input
-						class="input-elem"
-						placeholder="add more people..."
-						(keydown.enter)="handleEnterClick()"
-						(keydown.control.v)="handleCtrlVClick()"
-						(keydown.backspace)="handleBackspaceClick()"
-						(keydown)="keyDown($event)"
-						[(ngModel)]="emailName"
-					/>
-				</div>
-			</div>
-			<div class="actions-block">
-				<button (click)="addEmail()">Add email</button>
-				<button (click)="getEmailCount()">Get email count</button>
-			</div>
-		</div>
+        <div>
+            <div class="emailsEditor">
+                <div class="header">Share <strong>Board name</strong> with other</div>
+                <div class="emailBlock">
+                    <app-email-block
+                            *ngFor="let item of emailsArr"
+                            [emailObject]="item"
+                            (onDeleteClick)="onDeleteClick($event)"
+                    ></app-email-block>
+                    <input
+                            class="input-elem"
+                            placeholder="add more people..."
+                            (keydown.enter)="handleEnterClick()"
+                            (keydown.control.v)="handleCtrlVClick()"
+                            (keydown.backspace)="handleBackspaceClick()"
+                            (keydown)="keyDown($event)"
+                            [(ngModel)]="emailName"
+                    />
+                </div>
+            </div>
+            <div class="actions-block">
+                <button (click)="addEmail()">Add email</button>
+                <button (click)="getEmailCount()">Get email count</button>
+            </div>
+        </div>
 	`,
 	styleUrls: ['./emails-editor.component.less']
 })
@@ -52,19 +52,34 @@ export class EmailsEditorComponent {
 	private mailHostArr = ['yandex', 'mail', 'yahoo', 'gmail', 'rambler'];
 	private domainsArr = ['ru', 'com', 'au', 'at', 'az', 'al'];
 
-	private onDeleteClick(emailId: number) {
-		this.emailsArr = this.emailsArr.filter(item => item.id !== emailId);
-	}
-
-	private keyDown(event) {
+	public keyDown(event) {
 		if (event.code === 'Comma') {
 			this.addItem(this.emailName);
 			event.stopPropagation();
 		}
 	}
 
-	private handleEnterClick() {
+	public handleEnterClick() {
 		this.addItem(this.emailName);
+	}
+
+	public handleBackspaceClick() {
+		this.emailsArr.splice(-1, 1);
+	}
+
+	public addEmail() {
+		const randomName = this.makeRandomString(6);
+		const randHost = this.mailHostArr[Math.floor(Math.random() * this.mailHostArr.length)];
+		const randDomail = this.domainsArr[Math.floor(Math.random() * this.domainsArr.length)];
+		this.addItem(`${randomName}@${randHost}.${randDomail}`);
+	}
+
+	public getEmailCount() {
+		alert(`Кол-во email ${this.emailsArr.length}`);
+	}
+
+	private onDeleteClick(emailId: number) {
+		this.emailsArr = this.emailsArr.filter(item => item.id !== emailId);
 	}
 
 	private handleCtrlVClick() {
@@ -88,18 +103,6 @@ export class EmailsEditorComponent {
 		}, 1);
 	}
 
-	private handleBackspaceClick() {
-		this.emailsArr.splice(-1, 1);
-	}
-
-	private addEmail() {
-		console.log('add random email');
-		const randomName = this.makeRandomString(6);
-		const randHost = this.mailHostArr[Math.floor(Math.random() * this.mailHostArr.length)];
-		const randDomail = this.domainsArr[Math.floor(Math.random() * this.domainsArr.length)];
-		this.addItem(`${randomName}@${randHost}.${randDomail}`);
-	}
-
 	/**
 	 * Взято тут
 	 * https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
@@ -114,7 +117,5 @@ export class EmailsEditorComponent {
 		return text;
 	}
 
-	private getEmailCount() {
-		alert(`Кол-во email ${this.emailsArr.length}`);
-	}
+
 }
